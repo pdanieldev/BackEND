10989635792b9c2399d31fa5ca357536d50c5c12
Nathan <nathanlupus@gmail.com>
2026-09-10 12:57:49 -0300

    att django aula6

--- aula6/tarefas/views.py
+++ aula6/tarefas/views.py
@@ -1,22 +1,21 @@
-from urllib import request
-
-# Create your views here.
 from django.shortcuts import render
 
-from BackEND.aula6 import tarefas
+
 def inicio(request):
     return render(request, "tarefas/inicio.html")
+
+
 def lista_tarefas(request):
     tarefas = [
         {
-        "titulo": "Estudar URLs no Django",
-        "prioridade": "Alta",
-        "situacao": "Pendente",
+            "titulo": "Estudar URLs no Django",
+            "prioridade": "Alta",
+            "situacao": "Pendente",
         },
         {
-        "titulo": "Criar template de listagem",
-        "prioridade": "Média",
-        "situacao": "Concluída",
+            "titulo": "Criar template de listagem",
+            "prioridade": "Média",
+            "situacao": "Concluída",
         },
     ]
     return render(request, "tarefas/lista.html", {"tarefas": tarefas})
